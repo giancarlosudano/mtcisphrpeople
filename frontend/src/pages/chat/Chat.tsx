402759fa8829c994d00b616e719cbaea154048fe
Giancarlo Sudano <gisudano@microsoft.com>
2023-06-21 08:57:04 +0200

    Fix ShowAuthMessage (Autenticazione libera)

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -34,12 +34,12 @@ const Chat = () => {
     
     const getUserInfoList = async () => {
         const userInfoList = await getUserInfo();
-        if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
-            setShowAuthMessage(true);
-        }
-        else {
-            setShowAuthMessage(false);
-        }
+        // if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
+        //     setShowAuthMessage(true);
+        // }
+        // else {
+        setShowAuthMessage(false);
+        // }
     }
 
     const makeApiRequest = async (question: string) => {
